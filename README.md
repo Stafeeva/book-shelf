a3cf885592c0bd48eafa4d8098a9c04939e9180f
Natalia Stafeeva <n.stafeeva@gmail.com>
2019-05-18 11:16:30 +0200

    Handle post requests

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ### Book Shelf ###
 
 Express - MongoDB - React - Redux project
+
+### NOTES ###
+
+Curl POST request example:
+
+```curl -v http://localhost:3000/api/books -X POST -H "Content-Type: application/json" -d '{"key1":"value1", "key2":"value2"}'```
--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+
+app.use(bodyParser.json());
 
 app.get('/api/books', (req, res) => {
   res.json([
@@ -12,5 +16,11 @@ app.get('/api/books', (req, res) => {
     }
   ]);
 });
+
+app.post('/api/books', (req, res) => {
+  console.log('========req.body========', req.body);
+
+  res.sendStatus(201);
+})
 
 app.listen(port, () => console.log(`Bookshelf listening on port ${port}!`));
