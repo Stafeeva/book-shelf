ba836d79de2e4b0a3da4beffbfc4fb4cbac675ed
Natalia Stafeeva <n.stafeeva@gmail.com>
2019-07-03 17:38:05 +0200

    Add decorators to babel

--- client/components/AddBook.jsx
+++ client/components/AddBook.jsx
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import autobind from 'autobind-decorator';
 
 import { addBook } from '../actions';
 
 import './add-book.css';
 
 class AddBook extends Component {
+  constructor(props) {
+    super(props);
+
+     this.state = {
+       showBookField: false,
+     };
+  }
+
+  @autobind onClickAddBook() {
+    console.log('dispatching.....');
+    this.props.dispatch(addBook('1984'));
+  }
 
   render() {
-
-    const onClickAddBook = () => {
-      console.log('dispatching.....');
-      this.props.dispatch(addBook('1984'));
-    }
+    const { showAddressSearchField } = this.state;
+    const { onClickAddBook } = this;
 
     return (
       <div className="add-book">
